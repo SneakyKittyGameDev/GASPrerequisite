44f72415f4a3df69767b4f7529875eb9060855cb
Dakota Dawe <ddawe001@odu.edu>
2024-09-16 09:50:57 -0400

    P4 Bind Ability Inputs

--- Source/AbilitySystemCore/Public/Components/AbilitySystemEnhancedInputComponent.h
+++ Source/AbilitySystemCore/Public/Components/AbilitySystemEnhancedInputComponent.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "DataAssets/InputAbilitySystemDataAsset.h"
 #include "EnhancedInputComponent.h"
 #include "AbilitySystemEnhancedInputComponent.generated.h"
 
@@ -15,4 +16,17 @@ class ABILITYSYSTEMCORE_API UAbilitySystemEnhancedInputComponent : public UEnhan
 public:
 	// Sets default values for this component's properties
 	UAbilitySystemEnhancedInputComponent();
+
+	template <class UserClass, typename PressedFunction, typename ReleasedFunction>
+	void BindAbilityInputs(UInputAbilitySystemDataAsset* InputDataAsset, UserClass* Object, PressedFunction PressedFunctionCallback, ReleasedFunction ReleasedFunctionCallback);
 };
+
+template <class UserClass, typename PressedFunction, typename ReleasedFunction>
+void UAbilitySystemEnhancedInputComponent::BindAbilityInputs(UInputAbilitySystemDataAsset* InputDataAsset, UserClass* Object, PressedFunction PressedFunctionCallback, ReleasedFunction ReleasedFunctionCallback)
+{
+	for (const FAbilityInputAction& AbilityInputAction : InputDataAsset->AbilityInputs)
+	{
+		BindAction(AbilityInputAction.InputAction, ETriggerEvent::Started, Object, PressedFunctionCallback, AbilityInputAction.InputTag);
+		BindAction(AbilityInputAction.InputAction, ETriggerEvent::Completed, Object, ReleasedFunctionCallback, AbilityInputAction.InputTag);
+	}
+}
--- Source/AbilitySystemCore/Public/DataAssets/InputAbilitySystemDataAsset.h
+++ Source/AbilitySystemCore/Public/DataAssets/InputAbilitySystemDataAsset.h
@@ -3,14 +3,28 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 #include "Engine/DataAsset.h"
 #include "InputAbilitySystemDataAsset.generated.h"
 
-/**
- * 
- */
+
+class UInputAction;
+
+USTRUCT(BlueprintType)
+struct FAbilityInputAction
+{
+	GENERATED_BODY()
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	TObjectPtr<UInputAction> InputAction;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	FGameplayTag InputTag;
+};
+
 UCLASS()
 class ABILITYSYSTEMCORE_API UInputAbilitySystemDataAsset : public UDataAsset
 {
 	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Input")
+	TArray<FAbilityInputAction> AbilityInputs;
 };
