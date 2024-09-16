51b0039e89b4a92cf85912d2a26af4530eaf27be
Dakota Dawe <ddawe001@odu.edu>
2024-09-16 08:37:51 -0400

    Initial commit

---
+++ Source/GASTutorial/GASTutorialGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GASTutorialGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGASTutorialGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGASTutorialGameMode();
+};
+
+
+
